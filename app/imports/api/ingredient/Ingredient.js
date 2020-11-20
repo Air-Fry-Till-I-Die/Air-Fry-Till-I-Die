b3feecaf4adb567298cc7d70d8818f985fe91765
Alvin Yang <alvinyang101@gmail.com>
2020-11-19 22:00:38 -1000

    Finished Ingredient/Recipe collections

---
+++ app/imports/api/ingredient/Ingredient.js
@@ -0,0 +1,29 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class IngredientsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'RecipeCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      servings: { type: Number, defaultValue: 1 },
+      calories: Number,
+      vegetarian: { type: Boolean, defaultValue: false },
+      vegan: { type: Boolean, defaultValue: false },
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.publicPublicationName = `${this.name}.publications.public`;
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Ingredients = new IngredientsCollection();
