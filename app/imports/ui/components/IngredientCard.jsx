ef136e3c522c76106e0341d9cdb97336ead8c952
Thomas Yang <101thomasyang@gmail.com>
2020-12-01 20:45:39 -1000

    Fixed Nutritions

--- app/imports/ui/components/IngredientCard.jsx
+++ app/imports/ui/components/IngredientCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Image, Card } from 'semantic-ui-react';
+import { Image, Card, Modal, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class IngredientCard extends React.Component {
+  vegetarian = (this.props.ingredient.vegetarian) ? 'Yes' : 'No';
+
+  vegan = (this.props.ingredient.vegan) ? 'Yes' : 'No';
+
   render() {
     return (
       <Card centered>
@@ -13,9 +17,42 @@ class IngredientCard extends React.Component {
           <Card.Header>{this.props.ingredient.name}</Card.Header>
           <Card.Description>Servings: {this.props.ingredient.servings}</Card.Description>
         </Card.Content>
-        <Card.Content extra>
-          <Link to={'/nutrition/'}>Nutrition</Link>
-        </Card.Content>
+        <Modal
+            trigger={<Button>Show Nutrition</Button>}
+            header={this.props.ingredient.name}
+            image={this.props.ingredient.image}
+            content={
+              <div style={{ color: 'black', fontSize: 20, paddingLeft: 20, paddingRight: 20, paddingBottom: 20 }}>
+                <Image size='medium' src={this.props.ingredient.image} centered/>
+                Servings: {this.props.ingredient.servings} {this.props.ingredient.unit}
+                <hr/>
+                <p>
+                  Calories:&nbsp;
+                  {this.props.ingredient.calories}
+                  <br/>
+                  Fat:&nbsp;
+                  {this.props.ingredient.fat} g
+                  <br/>
+                  Carbs:&nbsp;
+                  {this.props.ingredient.carbs} g
+                  <br/>
+                  Protein:&nbsp;
+                  {this.props.ingredient.protein} g
+                  <br/>
+                  Sodium:&nbsp;
+                  {this.props.ingredient.sodium} mg
+                  <br/>
+                  Vegetarian?&nbsp;
+                  {this.vegetarian}
+                  <br/>
+                  Vegan?&nbsp;
+                  {this.vegan}
+                  <br/>
+                </p>
+              </div>
+            }
+            actions={[{ key: 'done', content: 'Done', positive: true }]}
+        />
       </Card>
     );
   }
