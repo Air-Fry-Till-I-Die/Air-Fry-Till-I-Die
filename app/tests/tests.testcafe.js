a4bae3bf75ddfd5158c857c1aa2c226cbefbc0e9
Alvin Yang <alvinyang101@gmail.com>
2020-12-16 23:27:25 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -3,8 +3,8 @@ import { signinPage } from './signin.page';
 import { signoutPage } from './signout.page';
 import { navBar } from './navbar.component';
 import { allRecipePage } from './allrecipe.page';
-import { addRecipePage } from './addrecipe.page';
-import { myRecipePage } from './myrecipe.page';
+// import { addRecipePage } from './addrecipe.page';
+// import { myRecipePage } from './myrecipe.page';
 import { myInventoryPage } from './myinventory.page';
 import { listIngredientsPage } from './listingredients.page';
 /* global fixture:false, test:false */
@@ -32,25 +32,25 @@ test('Test that All Recipes page is displayed', async (testController) => {
   await allRecipePage.isDisplayed(testController);
 });
 
-test('Test that Add Recipes, My Recipes, My Inventory page is displayed', async (testController) => {
+test('Test that My Inventory page and List Ingredients Page is displayed', async (testController) => {
   await navBar.gotoSigninPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.isLoggedIn(testController, credentials.username);
-  await navBar.gotoAddRecipePage(testController);
-  await addRecipePage.isDisplayed(testController);
-  await navBar.gotoMyRecipePage(testController);
-  await myRecipePage.isDisplayed(testController);
+  // await navBar.gotoAddRecipePage(testController);
+  // await addRecipePage.isDisplayed(testController);
+  // await navBar.gotoMyRecipePage(testController);
+  // await myRecipePage.isDisplayed(testController);
   await navBar.gotoMyInventoryPage(testController);
   await myInventoryPage.isDisplayed(testController);
   await navBar.gotoListIngredientsPage(testController);
   await listIngredientsPage.isDisplayed(testController);
 });
 
-test('Test that Add Recipes Form works', async (testController) => {
-  await navBar.gotoSigninPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
-  await navBar.isLoggedIn(testController, credentials.username);
-  await navBar.gotoAddRecipePage(testController);
-  await addRecipePage.addRecipe(testController, 'Pizza', '1', 'Delicious Pizza',
-      'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg', 'Dough, Pizza Sauce, Mozzarella', 'Put the pizza in the air fryer', true);
-});
+// test('Test that Add Recipes Form works', async (testController) => {
+//   await navBar.gotoSigninPage(testController);
+//   await signinPage.signin(testController, credentials.username, credentials.password);
+//   await navBar.isLoggedIn(testController, credentials.username);
+//   await navBar.gotoAddRecipePage(testController);
+//   await addRecipePage.addRecipe(testController, 'Pizza', '1', 'Delicious Pizza',
+//       'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg', 'Dough, Pizza Sauce, Mozzarella', 'Put the pizza in the air fryer', true);
+// });
